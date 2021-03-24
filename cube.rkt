e8cd4284b4b5a02962c9138d85da9090e5f2eb86
AlexDuchnowski <alex_duchnowski@brown.edu>
2021-03-23 21:29:01 -0400

    Made a few abstraction choices in a new file

---
+++ cube.rkt
@@ -0,0 +1,17 @@
+#lang forge
+
+abstract sig Color {}
+one sig White, Green, Orange, Red, Blue, Yellow extends Color {}
+
+abstract sig Position {}
+one TL, TM, TR, ML, MR, BL, BM, BR extends Position {}
+
+abstract sig Face {
+	stickers: set Position->Color
+	toup: one Face
+	todown: one Face
+	toleft: one Face
+	toright: one Face
+}
+
+one UFace, FFace, LFace, RFace, BFace, DFace extends Face {}
